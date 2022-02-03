3479d52f2015984d88a0117d5065a75984994012
Mdigennaro <michele.digennaro719@gmail.com>
2022-02-03 15:51:08 +0100

    fine show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <h1>Show</h1>
+  <div class="pt-5 container">
+
+    <div class="mdg-post text-center pb-5">
+
+      <h1 class="pb-5">{{$post->title}}</h1>
+  
+      <p>{{$post->content}}</p>
+
+    </div>  
+    <div class="btn-action">
+
+      <a href="{{route('admin.posts.index')}}" class="btn btn-info mr-2">Lista</a>
+      <a href="#" class="btn btn-warning">Modifica</a>
+      
     </div>
+
   </div>
 @endsection
